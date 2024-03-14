1f1dfecbe47cb6d8da940585dcf904ee34812985
Nora Kauczor <n.kauczor@web.de>
2024-03-14 11:39:36 -0700

    finish challenge 1

--- index.js
+++ index.js
@@ -8,10 +8,13 @@
 
 console.clear();
 
-const bookmarkButton = document.querySelector(`[data-js="card_button-bookmark"]`);
-console.log(bookmarkButton);
+const bookmarkButton = document.querySelector(
+  `[data-js="card__button-bookmark"]`
+);
 
-bookmarkButton.addEventListener("click", () => {bookmarkButton.classList.toggle("card__button-bookmark_black");});
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
 
 // function toggleColor(button){button.classList.toggle("card__button-bookmark_black");}
 
@@ -22,14 +25,25 @@ bookmarkButton.addEventListener("click", () => {bookmarkButton.classList.toggle(
 // When the user clicks this button again the answer is hidden again
 // The user can click on this button endlessly and the answer will either be displayed or hidden after each click
 
-const answerButton = document.querySelector(`[data-js="card_button-answer"]`);
+const answerButton = document.querySelector(`[data-js="card__button-answer"]`);
 
-const cardAnswer = document.querySelector(`[data-js="card_answer"]`);
+const cardAnswer = document.querySelector(`[data-js="card__answer"]`);
 
 answerButton.addEventListener("click", () => {
-    // The toggle functionality should be applied by using a class which is named "hidden"
-    cardAnswer.classList.toggle("hidden");
-// If the user clicks on an answer button, we want the button to say "hide answer" when the answer is displayed and "show answer" when the answer is not displayed.
-answerButton.textContent = "Hide answer";
-}
-)
+  // The toggle functionality should be applied by using a class which is named "hidden"
+  cardAnswer.classList.toggle("card__answer--active");
+  // If the user clicks on an answer button, we want the button to say "hide answer" when the answer is displayed and "show answer" when the answer is not displayed.
+  // answerButton.textContent = "Hide answer";
+  console.log(answerButton.textContent);
+  const isAnswerVisible = cardAnswer.classList.contains("card__answer--active");
+
+  if (isAnswerVisible) {
+    answerButton.textContent = "Hide answer";
+  } else {
+    answerButton.textContent = "Show answer";
+  }
+  console.log("---");
+  console.log(answerButton.textContent);
+});
+
+// advanceter wäre wenn man dann ein internes attribut vergibt z.b. "data-visible" und dann würde man die konstante, die man erstellt, also den boolen isAnswerVisible würde man gleich wenn der value von diesem attribut in diesem element "visible" ist (o.ä.)
